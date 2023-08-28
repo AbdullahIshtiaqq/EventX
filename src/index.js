504e1ac574273747d9a64949597d81471ebc8e5f
Abdullah Ishtiaq <abdullah.ishtiaq@prescribenow.com>
2023-08-28 18:35:55 +0500

    Project Starter

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./App/App.css";
+import App from "./App/App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootDiv = document.getElementById("root");
+
+function render() {
+  const root = ReactDOM.createRoot(rootDiv);
+  root.render(<App />);
+}
+
+if (module.hot) {
+  module.hot.accept("./App/App", function () {
+    setTimeout(render);
+  });
+}
+
+render();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
